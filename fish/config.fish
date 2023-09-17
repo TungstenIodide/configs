1869a357cf26c5c6a24d86af932d272c7e24f63d
Wiktoria <wiktoria.janas@pm.me>
2023-09-17 18:33:31 +0200

    init

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+abbr -a hx "helix"
+abbr -a enter "distrobox-enter"
+abbr -a pkg "sudo transactional-update pkg"
+
+fish_ssh_agent
