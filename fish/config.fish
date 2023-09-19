6c579605ffd24962c8b9237bacc2c6bcf231d282
Wiktoria <wiktoria.janas@pm.me>
2023-09-19 09:33:25 +0200

    Add xdg-open alias

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,9 @@ end
 
 abbr -a hx "helix"
 abbr -a e "helix"
+abbr -a o "xdg-open"
 abbr -a enter "distrobox-enter"
+abbr -a der "distrobox-enter rust"
 abbr -a pkg "sudo transactional-update pkg"
 abbr -a exp "export (cat .env |xargs -L 1)"
 abbr -a gcm "git commit -m"
