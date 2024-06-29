7c9a1fdf54e6350f8897240cf58ae7804fa4c79d
Wiktoria Janas <wiktoria.janas@pm.me>
2024-06-29 11:18:29 +0200

    Update aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,17 +2,16 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-abbr -a hx "helix"
-abbr -a e "helix"
-abbr -a o "xdg-open"
-abbr -a enter "distrobox-enter"
+abbr -a e hx
+abbr -a o xdg-open
+abbr -a de distrobox-enter
 abbr -a der "distrobox-enter rust"
 abbr -a dea "distrobox-enter arch"
 abbr -a pkg "sudo transactional-update pkg"
 abbr -a exp "export (cat .env |xargs -L 1)"
 abbr -a gcm "git commit -m"
 
-setenv EDITOR helix
+setenv EDITOR hx
 
 fish_add_path -aP ~/.local/bin
 
